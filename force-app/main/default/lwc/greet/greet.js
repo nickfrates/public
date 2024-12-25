b450ff5aaa1dd514b4bf44b3f60aff846d74005c
Nick Frates <nick@nickfrates.com>
2024-12-25 12:15:19 -0800

    Add 12/25 daily challenges

---
+++ force-app/main/default/lwc/greet/greet.js
@@ -0,0 +1,19 @@
+import { LightningElement, track } from 'lwc';
+
+export default class Greet extends LightningElement {
+
+    @track userName = '';
+    @track greeting = '';
+
+    handleNameChange(event){
+        this.userName = event.target.value;
+    }
+
+    handleClick(){
+        if (this.userName) {
+            this.greeting = `Hello, ${this.userName}!`;
+        } else {
+            this.greeting = 'Please enter a name';
+        }
+    }
+}
