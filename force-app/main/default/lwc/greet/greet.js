44437ee2b4b69821d145fac2a14ec57d552586dc
Nick Frates <nick@nickfrates.com>
2025-01-03 18:43:54 -0800

    Update Greet js/html

--- force-app/main/default/lwc/greet/greet.js
+++ force-app/main/default/lwc/greet/greet.js
@@ -1,24 +1,19 @@
 import { LightningElement, track } from 'lwc';
 
 export default class Greet extends LightningElement {
-
+    
     @track userName = '';
     @track greeting = '';
 
     handleNameChange(event){
         this.userName = event.target.value;
-        console.log('🔵 NAME CHANGE EVENT:', this.userName);
     }
 
     handleClick(){
-        console.log('🟢 BUTTON CLICKED!');
         if(this.userName){
             this.greeting = `Hello, ${this.userName}!`;
-            console.log('✅ Greeting set to:', this.greeting);
         } else {
             this.greeting = 'Please enter a name';
-            console.log('❌ No name entered');
         }
     }
-
 }
